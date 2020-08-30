c60b8cb0bea2a4b259ba86e1723b9f50c676a2f5
Yoga Sukma <hello@yogasukma.web.id>
2020-08-30 09:20:45 +0800

    init file

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const {src, dest, parallel, watch} = require('gulp');
+const pug                          = require('gulp-pug');
+const sass                         = require('gulp-sass');
+const minifyCSS                    = require('gulp-csso');
+const concat                       = require('gulp-concat');
+
+var srcDir  = "src";
+var distDir = "dist";
+
+function html() {
+    return src(srcDir + '/pug/pages/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(dest(distDir))
+}
+
+function css() {
+    return src(srcDir + '/scss/*.scss')
+        .pipe(sass())
+        .pipe(minifyCSS())
+        .pipe(dest( distDir + '/css'))
+}
+
+function js() {
+    return src([
+        srcDir + "/js/app.js",
+    ], {sourcemaps: true})
+        .pipe(concat('app.min.js'))
+        .pipe(dest( distDir + '/js', {sourcemaps: true}))
+}
+
+
+function images() {
+    return src(srcDir + '/images/**/*.*')
+        .pipe(dest(distDir + '/images'));
+
+}
+
+function plugins() {
+    return src(srcDir + '/plugins/**/*.*')
+        .pipe(dest(distDir + '/plugins'));
+}
+
+function listen() {
+    return watch(srcDir + "/**/*", parallel(html, css, js, images));
+}
+
+exports.html   = html;
+exports.css    = css;
+exports.js     = js;
+exports.images = images;
+exports.plugins = plugins;
+exports.listen  = listen;
+
+exports.default = parallel(html, css, js, images, plugins, listen);
